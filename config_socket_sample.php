ee9a85c5fb65b4f39ca1a6fb5d44443d8d80235e
datagutten <datagutten@datagutten.net>
2015-08-07 21:11:34 +0200

    Fixed type in sample config

--- config_socket_sample.php
+++ config_socket_sample.php
@@ -1,4 +1,4 @@
 <?Php
-$host='192.168.0.50'; //IP address or hostname to the decoder
+$address='192.168.0.50'; //IP address or hostname to the decoder
 $port='5403'; //TCP port for the decoder, usually 5403
 $file_path='socket_data'; //Where to save the captured data
