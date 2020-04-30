d0325a8ce7adfd80ea7be80cdb731abcd969455c
datagutten <datagutten@datagutten.net>
2020-04-30 21:13:03 +0200

    Make parser class static and add PHPDoc and tests

--- amb-p3-parser.class.php
+++ amb-p3-parser.class.php
@@ -1,8 +1,10 @@
 <?Php
 class amb_p3_parser
 {
-	//Record types
-	public $record_types=array(	0x00=>'RESET',
+    /**
+     * @var array Record types
+     */
+	public static $record_types=array(	0x00=>'RESET',
 								0x01=>'PASSING',
 								0x02=>'STATUS',
 								0x45=>'FIRST_CONTACT',
@@ -21,8 +23,11 @@ class amb_p3_parser
 								0x4a=>'TIMELINE',
 								0x24=>'GET_TIME',
 								0x16=>'NETWORK_SETTINGS');
-	//Message field definitions
-	public $messages=array(0x01=>array(	0x01=>'PASSING_NUMBER',
+
+    /**
+     * @var array Message field definitions
+     */
+	public static $messages=array(0x01=>array(	0x01=>'PASSING_NUMBER',
 										0x03=>'TRANSPONDER',
 										0x04=>'RTC_TIME',
 										0x05=>'STRENGTH',
@@ -39,18 +44,27 @@ class amb_p3_parser
 										0x85=>'REQUEST_ID')
 									);
 
-	function trim_data($data,$debug=false) //Trim data to only contain complete records
+    /**
+     * Trim data to only contain complete records
+     * @param $data
+     * @return bool|string
+     */
+	public static function trim_data($data)
 	{
-		//var_dump(dechex(ord(substr($data,0,1))));
-		//var_dump(dechex(ord(substr($data,-1,1))));
-		$start=strpos($data,chr(0x8E));
-		$end=strrpos($data,chr(0x8F));
+		$start=self::find_start($data);
+		$end=self::find_last_end($data);
 		return substr($data,$start,$end-$start+1); //Trim the string to get only complete messages
 	}
-	function unescape($record) //Subtract 0x20 from any byte prefixed with 0x8D
+
+    /**
+     * Subtract 0x20 from any byte prefixed with 0x8D
+     * @param string $record
+     * @return string Record with unescaped bytes
+     */
+	public static function unescape($record)
 	{
 		$pos=-2;
-		while($pos=strpos($record,0x8D,$pos+2)) //Search from previous position +2 in case the unescaped byte is 0x8D
+		while($pos=strpos($record,chr(0x8D),$pos+2)) //Search from previous position +2 in case the unescaped byte is 0x8D
 		{
 			$escaped_char=ord(substr($record,$pos+1,1)); //Get the escaped char
 			$record=substr_replace($record,chr($escaped_char-0x20),$pos,2); //Remove the escape char and subtract 0x20 from the escaped char
@@ -69,7 +83,14 @@ class amb_p3_parser
 		return $records_preg[0];
 	}
 
-	function format_value($string,$return_as_hex=false,$reverse=true) //Convert a string to character codes
+    /**
+     * Convert a string to character codes
+     * @param $string String to be converted
+     * @param bool $return_as_hex Return as string with hex values or a decimal integer
+     * @param bool $reverse Reverse the byte order
+     * @return int|string
+     */
+	public static function format_value($string,$return_as_hex=false,$reverse=true)
 	{
 		if($reverse)
 			$string=strrev($string); //The values need to be reversed
@@ -87,22 +108,32 @@ class amb_p3_parser
 			return $number_hex; //Return as hex string
 	}
 
-	function parse_header($record) //Parse the record header
+    /**
+     * Parse the record header
+     * @param string $record Trimmed record
+     * @return array
+     */
+	public static function parse_header($record)
 	{
 		//Header
 		$info['version']=ord(substr($record,0x1,1));
-		$info['length']=$this->format_value(substr($record,0x2,2));
-		$info['crc']=$this->format_value(substr($record,0x4,2),true);
-		$info['flags_header']=$this->format_value(substr($record,0x6,2),true); //Indicates the field length
-		$info['type']=$this->format_value(substr($record,0x8,2)); //Get record type
-		$info['record_hex']=$this->format_value($record,true,false); //Get the complete record as a readable hex string
+		$info['length']=self::format_value(substr($record,0x2,2));
+		$info['crc']=self::format_value(substr($record,0x4,2),true);
+		$info['flags_header']=self::format_value(substr($record,0x6,2),true); //Indicates the field length
+		$info['type']=self::format_value(substr($record,0x8,2)); //Get record type
+		$info['record_hex']=self::format_value($record,true,false); //Get the complete record as a readable hex string
 		return $info;
 	}
-	function read_fields($record,$fields) //Read the fields of a record according to an array with field definations
-	{
-		$record_key=0;
-		$strlen=strlen($record);
 
+    /**
+     * Read the fields of a record according to an array with field definitions
+     * @param $record
+     * @param $fields
+     * @return mixed
+     */
+    public static function read_fields($record,$fields)
+	{
+		$messages = [];
 		for($pos=0xA; $pos<strlen($record)-1; $pos++) //Parse body
 		{
 			$field_id=ord($record[$pos]); //The first byte of a message is the field ID
@@ -115,26 +146,30 @@ class amb_p3_parser
 			$field_name=$fields[$field_id]; //Get the field name
 			$length=ord(substr($record,$pos+1,1)); //After the field ID we find the message length
 			//echo "Length: $length\n";
-			$messages[$field_name]=$this->format_value(substr($record,$pos+2,$length));
+			$messages[$field_name]=self::format_value(substr($record,$pos+2,$length));
 			$pos=$pos+$length+1;
 		}
 			
 		return $messages;
 	}
-	function parse($record,$typefilter=false) //Parse a record
+
+    /**
+     * Parse a record
+     * @param string $record
+     * @param bool $typefilter
+     * @return array|bool Returns an array for a valid record, returns bool false for errors, returns bool true for records skipped by type filter
+     */
+	public static function parse($record,$typefilter=false) //Parse a record
 	{
-		//Returns an array for a valid record
-		//Returns bool false for errors
-		//Returns bool true for records skipped by type filter
-		$record=$this->unescape($record);
+		$record=self::unescape($record);
 		if(ord(substr($record,0,1))!=0x8E || ord(substr($record,-1,1))!=0x8F) //Verify that the provided string is a complete record
 		{
 			trigger_error("Invalid record");
 			return false;
 		}
-		$header=$this->parse_header($record);
+		$header=self::parse_header($record);
 		
-		if(!isset($this->record_types[$header['type']]))
+		if(!isset(self::$record_types[$header['type']]))
 		{
 			trigger_error("Unkown record type: ".dechex($header['type']));
 			return false;
@@ -144,10 +179,10 @@ class amb_p3_parser
 		if($typefilter!==false && $header['type']!=$typefilter) //Type is not wanted
 			return true;
 
-		if(isset($this->messages[$header['type']])) //Check if the message can be parsed
+		if(isset(self::$messages[$header['type']])) //Check if the message can be parsed
 		{
-			$fields=$this->messages[$header['type']] + $this->messages['general'];
-			$message=$this->read_fields($record,$fields) ;//Read the fields of the record
+			$fields=self::$messages[$header['type']] + self::$messages['general'];
+			$message=self::read_fields($record,$fields) ;//Read the fields of the record
 			$record_parsed=array_merge($header,$message); //Merge the header and the the message body
 		}
 		else
@@ -159,16 +194,31 @@ class amb_p3_parser
 		return $record_parsed;
 	}
 
+    /**
+     * Find the first start byte
+     * @param string $record
+     * @return bool|int
+     */
 	public static function find_start($record)
     {
         return strpos($record,chr(0x8E));
     }
 
+    /**
+     * Find the first end byte
+     * @param string $record
+     * @return bool|int
+     */
     public static function find_end($record)
     {
         return strpos($record,chr(0x8F));
     }
 
+    /**
+     * Find the last end byte
+     * @param string $record
+     * @return bool|int
+     */
     public static function find_last_end($record)
     {
         return strrpos($record,chr(0x8F));
---
+++ tests/amb_p3_parserTest.php
@@ -0,0 +1,129 @@
+<?php
+
+
+use amb_p3_parser as parser;
+use PHPUnit\Framework\TestCase;
+
+
+class amb_p3_parserTest extends TestCase
+{
+
+    public function testTrim_data()
+    {
+        $trimmed = parser::trim_data(sprintf('asdf%1$sdfgfg%2$sllkk%1$sff%2$skk', chr(0x8E), chr(0x8F)));
+        $this->assertEquals(chr(0x8E).'dfg', substr($trimmed, 0,4));
+        $this->assertEquals('ff'.chr(0x8F), substr($trimmed, -3, 3));
+
+        $data = file_get_contents(__DIR__.'/test_data/need_trimming');
+        $trimmed = parser::trim_data($data);
+        $this->assertEquals(chr(0x8E).chr(0x02).chr(0x33), substr($trimmed, 0,3));
+        $this->assertEquals(chr(0x02).chr(0x00).chr(0x8F), substr($trimmed, -3,3));
+    }
+
+    public function testRead_fields()
+    {
+        $data = file_get_contents(__DIR__.'/test_data/good_passing');
+        $fields=parser::$messages[1] + parser::$messages['general'];
+        $record = parser::read_fields($data, $fields);
+        $fields_ref = [
+            'PASSING_NUMBER'=>113736,
+            'TRANSPONDER'=>2906141,
+            'RTC_TIME'=>1437769372993000,
+            'STRENGTH'=>106,
+            'HITS'=>50,
+            'FLAGS'=>0,
+            'DECODER_ID'=>135059];
+        $this->assertEquals($fields_ref, $record);
+    }
+
+    public function testParse()
+    {
+        $data = file_get_contents(__DIR__.'/test_data/good_passing');
+        $passing = parser::parse($data);
+        $passing_ref = [
+            'version'=>2,
+            'length'=>51,
+            'crc'=>'c79e',
+            'flags_header'=>0000,
+            'type'=>1,
+            'record_hex'=>'8e0233009ec700000100010448bc010003041d582c000408e805bfc4a41b050005026a0006023200080200008104930f02008f',
+            'PASSING_NUMBER'=>113736,
+            'TRANSPONDER'=>2906141,
+            'RTC_TIME'=>1437769372993000,
+            'STRENGTH'=>106,
+            'HITS'=>50,
+            'FLAGS'=>0,
+            'DECODER_ID'=>135059
+        ];
+        $this->assertEquals($passing_ref, $passing);
+    }
+
+    public function testInvalidRecord()
+    {
+        $this->expectError();
+        $this->expectErrorMessage('Invalid record');
+        parser::parse('asdf');
+    }
+
+    public function testParse_header()
+    {
+        $data = file_get_contents(__DIR__.'/test_data/good_passing');
+        $header = parser::parse_header($data);
+        $header_ref = [
+            'version'=>2,
+            'length'=>51,
+            'crc'=>'c79e',
+            'flags_header'=>0000,
+            'type'=>1,
+            'record_hex'=>'8e0233009ec700000100010448bc010003041d582c000408e805bfc4a41b050005026a0006023200080200008104930f02008f'
+        ];
+        $this->assertEquals($header_ref, $header);
+    }
+
+    public function testUnescape()
+    {
+        $data = file_get_contents(__DIR__.'/test_data/1437857960_0');
+        $record = parser::unescape($data);
+        $this->assertEquals(chr(0x8F), substr($record, 5,1));
+    }
+
+    public function testFormat_value()
+    {
+        $string = parser::format_value(chr(0x0F));
+        $this->assertEquals(15, $string);
+
+        $string = parser::format_value(chr(0x0F).chr(0xFF));
+        $this->assertEquals(0xFF0F, $string);
+
+        $string = amb_p3_parser::format_value(chr(0x0F).chr(0xFF), false, false);
+        $this->assertEquals(0x0FFF, $string);
+
+        $string = parser::format_value(chr(0x0F).chr(0xFF), true);
+        $this->assertEquals('ff0f', $string);
+    }
+
+    public function testGet_records()
+    {
+        $data = file_get_contents(__DIR__.'/test_data/multi');
+        $records = parser::get_records($data);
+        $this->assertCount(10, $records);
+    }
+
+    public function testFind_start()
+    {
+        $data = file_get_contents(__DIR__.'/test_data/need_trimming');
+        $this->assertEquals(9, parser::find_start($data));
+    }
+
+    public function testFind_end()
+    {
+        $data = file_get_contents(__DIR__.'/test_data/multi');
+        $this->assertEquals(50, parser::find_end($data));
+    }
+
+    public function testFind_last_end()
+    {
+        $data = file_get_contents(__DIR__.'/test_data/multi');
+        $this->assertEquals(509, parser::find_last_end($data));
+    }
+}
