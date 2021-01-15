a7f48d6ea29490222cdf8265c636f5a446130cbf
datagutten <datagutten@datagutten.net>
2021-01-15 20:42:22 +0100

    Rewrite capture script and change config structure

--- config_socket_sample.php
+++ config_socket_sample.php
@@ -1,4 +1,5 @@
 <?Php
-$address='192.168.0.50'; //IP address or hostname to the decoder
-$port='5403'; //TCP port for the decoder, usually 5403
-$file_path='socket_data'; //Where to save the captured data
+$config['address'] = '192.168.0.50'; //IP address or hostname to the decoder
+$config['port'] = '5403'; //TCP port for the decoder, usually 5403
+
+return $config;
--- socket.php
+++ socket.php
@@ -1,24 +1,21 @@
 <?Php
 //Capture data from a AMB decoder using a socket
-if(isset($argv[1]) && file_exists($conf="config_socket_{$argv[1]}.php"))
-	require $conf;
-else
-	require 'config_socket.php';
-if(!file_exists($file_path))
-	mkdir($file_path);
+require 'vendor/autoload.php';
+use datagutten\amb\parser\socket;
 
-$socket=socket_create(AF_INET,SOCK_STREAM,0);
-$result = socket_connect($socket,$address,$port) or die("Could not connect to server\n");
+if(isset($argv[1]) && file_exists($conf="config_socket_{$argv[1]}.php"))
+	$config = require $conf;
+else
+	$config = require 'config_socket.php';
+
+$file_path = __DIR__.'/socket_data/'.$argv[1];
+if(!file_exists($file_path))
+	mkdir($file_path, 0777, true);
+
+$socket = new socket($config['address'], $config['port']);
 
 while (true) {
-    $record = socket_read ($socket, 1024);
-	if($record===false)
-	{
-		echo "Could not read server response\n";
-		continue;
-	}
-	if(empty($record))
-		continue;
+    $record = $socket->read();
 
 	$type=dechex(ord(substr($record,0x08,1)));
 	if(ord(substr($record,0,1))!=0x8E || ord(substr($record,-1,1))!=0x8F || $type!=0x02) //Do not save packets with only status message
